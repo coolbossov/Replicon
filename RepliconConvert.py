9f3f616986eb22f5abf0e2f70c2530241483de55
ausov <coolbossov@gmail.com>
2016-08-02 13:59:09 +0300

    Using library instead to add employee ID and amount of work / days

--- RepliconConvert.py
+++ RepliconConvert.py
@@ -1,8 +1,6 @@
-
-
-
-
-source_file = open("/Users/ausov/Desktop/report.csv", "r")
+#This script receives a csv report from replicon
+import boto3
+source_file = open("/Users/ausov/Desktop/report2.csv", "r")
 output_file = open("/Users/ausov/Desktop/daysoff.csv", "w")
 
 # Separate the csv file, every line to an list
@@ -11,42 +9,38 @@ for line in source_file.readlines():
     array.append(line)
 
 # Separate every line words into an list, put in a list arrays
+
 num = 0
 arrays = []
 for i in array:
     arrays.append(array[num].split(",")) #create an array of arrays  - all parameters"
-    print array[num]
+    # print array[num]
     num = num + 1
 
 
-#print the user ID and the amount of his vacation / sick / miluim  days into a csv file
+#Create a dictionary with the IDs of the user and add to it a dictionary of the vacation/sick days
 
-num = 0
-vacation = 0
-miluim = 0
-sick = 0
-
+people_dic = {}
 for i in arrays:
     if i[0].isdigit and not len(i[0]) == 0 and not i[0] == "Employee ID":
-        output_file.write("%s , %s , %s , %s \n" % (id, vacation, sick, miluim))
-        id =  (i[0])
-        print ("%s , %s , %s , %s \n" % (id, vacation, sick, miluim))
-        vacation = 0
-        miluim = 0
-        sick = 0
+        id = (i[0])  #The ID of the person
+        people_dic[id] = {
+            'vacation' : 0,
+            'sick' : 0,
+            'miluim' : 0,
+        }
+
     elif i[0].isalpha and not len(i[1]) == 0:
         if i[3] == "Vacation":
-            vacation = vacation + 1
-            print ("%s , %s ") % (id, vacation)
+            people_dic[id]['vacation']  = people_dic[id]['vacation'] + 1
         elif i[3] == "Sick":
-            sick = sick + 1
-            print ("%s , %s ") % (id, sick)
+            people_dic[id]['sick'] = people_dic[id]['sick'] + 1
         elif i[3] == "Army reserve":
-            miluim = miluim + 1
-            print ("%s , %s ") % (id, miluim)
+            people_dic[id]['miluim'] = people_dic[id]['miluim'] + 1
+
+for d in people_dic:
+    output_file.write("%s,%s,%s,%s \n" % (d, people_dic[d]['vacation'], people_dic[d]['sick'], people_dic[d]['miluim']))
 
 
 output_file.close()
 
-    # "arrays[num][0] = arrays[num][0].replace(",,,,,,", "")
-# todo add 0 in from of the ID
