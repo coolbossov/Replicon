191c4298d8235b693f68a8f1445d7d1e660c27e5
ausov <coolbossov@gmail.com>
2016-08-07 17:28:50 +0300

    Clean up use functions use variables use constants
    Version 1 is done

--- RepliconConvert.py
+++ RepliconConvert.py
@@ -1,46 +1,79 @@
-#This script receives a csv report from replicon
-import boto3
-source_file = open("/Users/ausov/Desktop/report2.csv", "r")
-output_file = open("/Users/ausov/Desktop/daysoff.csv", "w")
-
-# Separate the csv file, every line to an list
-array = []
-for line in source_file.readlines():
-    array.append(line)
-
-# Separate every line words into an list, put in a list arrays
-
-num = 0
-arrays = []
-for i in array:
-    arrays.append(array[num].split(",")) #create an array of arrays  - all parameters"
-    # print array[num]
-    num = num + 1
+# This script receives 2 csv report from replicon
+# The ID of the user must be the first row in each row - checks if 0 is missing in the ID and adds it
+from os import path
+import calendar
 
 
-#Create a dictionary with the IDs of the user and add to it a dictionary of the vacation/sick days
+##################Globlas#########################
 
-people_dic = {}
-for i in arrays:
-    if i[0].isdigit and not len(i[0]) == 0 and not i[0] == "Employee ID":
-        id = (i[0])  #The ID of the person
-        people_dic[id] = {
-            'vacation' : 0,
-            'sick' : 0,
-            'miluim' : 0,
-        }
+VACATION = "Vacation"
+SICK = "Sick"
+MILUIM = "Army reserve"
+BASE_PATH = "/Users/ausov/Desktop"
+SOURCE_FILE_DAYSOFF = "daysoff.csv"
+SOURCE_FILE_TOTALS = "total.csv"
+OUTPUT_FILE = "Fundbox_Time_Summery.csv"
 
-    elif i[0].isalpha and not len(i[1]) == 0:
-        if i[3] == "Vacation":
-            people_dic[id]['vacation']  = people_dic[id]['vacation'] + 1
-        elif i[3] == "Sick":
-            people_dic[id]['sick'] = people_dic[id]['sick'] + 1
-        elif i[3] == "Army reserve":
-            people_dic[id]['miluim'] = people_dic[id]['miluim'] + 1
+##################################################
 
-for d in people_dic:
-    output_file.write("%s,%s,%s,%s \n" % (d, people_dic[d]['vacation'], people_dic[d]['sick'], people_dic[d]['miluim']))
+source_file_daysoff = open(path.join(BASE_PATH, SOURCE_FILE_DAYSOFF), "r")
+source_file_total_days = open(path.join(BASE_PATH, SOURCE_FILE_TOTALS), "r")
+output_file = open(path.join(BASE_PATH,OUTPUT_FILE), "w")
+
+
+###################Functions##############################
+
+def seperate_csv_to_lines(csv_file):
+    seperated_list_of_lists = []
+    for csv_line in csv_file.readlines():
+        split_line = csv_line.split(",")
+        if len(split_line[0]) < 9 and len(split_line[0]) > 3: # If the ID is 8 digit and the "0" was removed for some reason, add it back
+            split_line[0] = "0" + split_line[0]
+        seperated_list_of_lists.append(split_line)
+    return seperated_list_of_lists
+
+
+def count_days(list_of_ids_and_daysoff, list_of_ids_and_total):
+    dict_daysoff = {}
+    for line in list_of_ids_and_total:
+        if line[0].isdigit and len(line[0]) > 3 and not line[0] == "Employee ID": # Add the total days and hours for every ID
+            dict_daysoff[line[0]] = {
+                'vacation': 0,
+                'sick': 0,
+                'miluim': 0,
+                'total_days': line[2].rstrip(),
+                'total_hours': line[1],
+             }
+
+    for line in list_of_ids_and_daysoff:
+        if line[0].isdigit and len(line[0]) > 3 and not line[0] == "User First Name" and not line[0] == "\r\n": # Add the off days for every ID
+            if line[3] == VACATION:
+                dict_daysoff[line[0]]['vacation'] += 1
+            elif line[3] == SICK:
+                dict_daysoff[line[0]]['sick'] += 1
+            elif line[3] == MILUIM:
+                dict_daysoff[line[0]]['miluim'] += 1
+    return dict_daysoff
+
+
+
+days_off_list = seperate_csv_to_lines(source_file_daysoff)
+total_day_list = seperate_csv_to_lines(source_file_total_days)
+dict_of_days_off_and_total = count_days(days_off_list,total_day_list)
+
+
+############################  Create the new CSV file #######################
+
+month_dict = {v: k for k,v in enumerate(calendar.month_name)}
+output_file.write("004, %s, %s \n" % (total_day_list[1][4].rstrip(),month_dict[total_day_list[1][3]]))
+
+for d in dict_of_days_off_and_total:
+    #print ("%s,%s,%s,%s,%s,%s\n" % (d, dict_of_days_off_and_total[d]['total_days'],dict_of_days_off_and_total[d]['total_hours'], dict_of_days_off_and_total[d]['vacation'], dict_of_days_off_and_total[d]['sick'],dict_of_days_off_and_total[d]['miluim']))
+    output_file.write("%s,%s,%s,%s,%s,%s\n" % (d, dict_of_days_off_and_total[d]['total_days'],dict_of_days_off_and_total[d]['total_hours'], dict_of_days_off_and_total[d]['vacation'], dict_of_days_off_and_total[d]['sick'],dict_of_days_off_and_total[d]['miluim']))
+
 
 
 output_file.close()
+
+
 
